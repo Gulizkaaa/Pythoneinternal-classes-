cbc1bc89aa630303326d5d078c865b234db5743a
gulizkaaa <guliza.almazbek@gmail.com>
2023-11-16 23:51:39 +0000

    changed file structure and naming

---
+++ Day1/nameandage.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name:")
+age = int(input ("Plese enter your age:")) 
+x = 2023-age 
+print("Hello", name) #do not forget highlight the variables. System should see the difference between variable and string.
+print("You are currently:", age)
+print(f"Your birth year is {x}") #new feature for myself if you are using f you may separate variable with two curly braces.
--- Day1/test.py
+++ Day1/test.py
@@ -7,3 +7,6 @@ y=4
 #print (x // y)
 #print (x % y)
 #print (x ** y)
+
+# string = "hello"
+# print(string * 5)
